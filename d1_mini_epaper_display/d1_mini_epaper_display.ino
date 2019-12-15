ea3695cc9f8bd34c686152f68ed0b55b10a7a5ae
mycognosist <mycognosist@cryptolab.net>
2019-12-15 19:28:27 +0000

    Initial commit with working example

---
+++ d1_mini_epaper_display/d1_mini_epaper_display.ino
@@ -0,0 +1,61 @@
+#include <Adafruit_SPITFT.h>
+#include <gfxfont.h>
+#include <Adafruit_SPITFT_Macros.h>
+#include <Adafruit_GFX.h>
+
+#include <GxFont_GFX.h>
+
+// pin mapping from Waveshare SPI e-Paper to Wemos D1 mini
+// BUSY -> D2, RST -> D4, DC -> D3, CS -> D8, CLK -> D5, DIN -> D7, GND -> GND, 3.3V -> 3.3V
+
+// include library, include base class, make path known
+#include <GxEPD.h>
+
+// select the display class to use, only one
+#include <GxGDEW0213I5F/GxGDEW0213I5F.h>  // 2.13" b/w 104x212 flexible
+
+#include GxEPD_BitmapExamples
+
+// FreeFonts from Adafruit_GFX
+#include <Fonts/FreeMonoBold9pt7b.h>
+//#include <Fonts/FreeMonoBold12pt7b.h>
+//#include <Fonts/FreeMonoBold18pt7b.h>
+//#include <Fonts/FreeMonoBold24pt7b.h>
+
+#include <GxIO/GxIO_SPI/GxIO_SPI.h>
+#include <GxIO/GxIO.h>
+
+GxIO_Class io(SPI, /*CS=D8*/ SS, /*DC=D3*/ 0, /*RST=D4*/ 2); // default selection of D3(=0), D4(=2)
+GxEPD_Class display(io, /*RST=D4*/ 2, /*BUSY=D2*/ 4); // default selection of D4(=2), D2(=4)
+
+void writeDisplay(const char name[], const GFXfont* f)
+{
+  display.fillScreen(GxEPD_WHITE);
+  display.setTextColor(GxEPD_BLACK);
+  display.setFont(f);
+  display.setCursor(0, 0);
+  display.println();
+  display.println(name);
+  display.update();
+  delay(5000);
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("setup");
+
+  display.init(115200); // enable diagnostic output on Serial
+
+  Serial.println("setup done");
+}
+
+void loop()
+{
+  writeDisplay("hello", &FreeMonoBold9pt7b);
+  //showFont("FreeMonoBold12pt7b", &FreeMonoBold12pt7b);
+  //showFont("FreeMonoBold18pt7b", &FreeMonoBold18pt7b);
+  //showFont("FreeMonoBold24pt7b", &FreeMonoBold24pt7b);
+  delay(10000);
+}
